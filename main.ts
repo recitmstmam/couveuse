0e3dfc45790d6cb824225f5e0bd5aa157daa4e2c
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-03-29 13:32:19 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ basic.forever(function () {
     while (Angle < 80) {
         Angle += 1
         pins.servoWritePin(AnalogPin.P0, Angle)
-        basic.pause(43200000)
+        basic.pause(100)
     }
-    basic.pause(100)
+    basic.pause(43200000)
     while (Angle > 35) {
         Angle += -1
         pins.servoWritePin(AnalogPin.P0, Angle)
-        basic.pause(43200000)
+        basic.pause(100)
     }
-    basic.pause(100)
+    basic.pause(43200000)
 })

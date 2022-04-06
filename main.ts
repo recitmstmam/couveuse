572e04e43fffbadf588285ca3df96fccf1e4292b
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-04-06 13:54:59 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (Environment.octopus_BME280(Environment.BME280_state.BME280_humidity)))
+})
 let Angle = 35
 pins.servoWritePin(AnalogPin.P0, Angle)
+pins.digitalWritePin(DigitalPin.P1, 0)
 basic.forever(function () {
-    while (Angle < 80) {
+    while (Angle < 75) {
         Angle += 1
         pins.servoWritePin(AnalogPin.P0, Angle)
         basic.pause(100)
@@ -13,4 +17,11 @@ basic.forever(function () {
         basic.pause(100)
     }
     basic.pause(43200000)
+})
+basic.forever(function () {
+    if (Environment.octopus_BME280(Environment.BME280_state.BME280_humidity) >= 44) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
 })
